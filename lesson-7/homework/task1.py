cc6fc1a9df733d2ee1ee9607dd0570025ea3ac7f
Nodir Fayzullayev <nodirfayz18@gmail.com>
2025-02-26 19:56:33 +0500

    some changes

--- lesson-7/homework/task1.py
+++ lesson-7/homework/task1.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
+
 # 1
-'''
+
 
 
 def is_prime(n):
@@ -12,7 +12,7 @@ def is_prime(n):
         return True
 
 print(is_prime(3))
-'''
+
 
 
 
@@ -35,45 +35,3 @@ print(is_prime(3))
 
 
 
-
-
-=======
-# 1
-'''
-
-
-def is_prime(n):
-    
-    for i in range(2,n):
-        if n % i == 0:
-            return False
-    else:
-        return True
-
-print(is_prime(3))
-'''
-
-
-
-# 2
-
-
-
-
-
-   
-
-
-
-
-
-
-# 3
-
-
-
-
-
-
-
->>>>>>> cf8d6416b90c8d8e28c2db3a2b57789798afffb2
